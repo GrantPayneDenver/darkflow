bc9770754d4ba57336a5eaec537a949fde81f74b
Grant Payne <grantitup@gmail.com>
2018-11-20 18:03:49 -0700

    got blacked out image frames for each bounding box - Grant

--- darkflow/net/yolov2/predict.py
+++ darkflow/net/yolov2/predict.py
@@ -3,6 +3,7 @@ import math
 import cv2
 import os
 import json
+import copy
 #from scipy.special import expit
 #from utils.box import BoundBox, box_iou, prob_compare
 #from utils.box import prob_compare2, box_intersection
@@ -15,10 +16,7 @@ global out_dir
 good_path = False
 while not good_path:
 	out_dir = input("Please enter a directory to store your output for bounding boxes")
-	if os.path.isdir(out_dir):
-		good_path = True
-	else:
-		print("Invalid path")
+	if os.path.isdir(out_dir): good_path = True
 
 from ...cython_utils.cy_yolo2_findboxes import box_constructor
 
@@ -41,6 +39,9 @@ def postprocess(self, net_out, im, save = True):
 	"""
 	Takes net output, draw net_out, save to disk
 	"""
+
+	saved_boxes = []
+
 	global frame
 	frame += 1
 	print('frame: ', frame)
@@ -72,11 +73,23 @@ def postprocess(self, net_out, im, save = True):
 			resultsForJSON.append({"label": mess, "confidence": float('%.2f' % confidence), "topleft": {"x": left, "y": top}, "bottomright": {"x": right, "y": bot}})
 			continue
 
+		# save the image itself for each box
+		image_copy = copy.copy(imgcv)
+		# black out all of the image except for the box contained in left,top and right,bot
+		black = (0,0,0)
+		fill  = -1
+
 		cv2.rectangle(imgcv,
 			(left, top), (right, bot),
 			colors[max_indx], thick)
 		cv2.putText(imgcv, mess, (left, top - 12),
 			0, 1e-3 * h, colors[max_indx],thick//3)
+
+		cv2.rectangle(image_copy, (0,0), (w, top), black, fill)
+		cv2.rectangle(image_copy, (0,top), (left, bot), black, fill)
+		cv2.rectangle(image_copy, (0,bot), (w, h), black, fill)
+		cv2.rectangle(image_copy, (right,top), (w, bot), black, fill)
+
 
 		img_num += 1
 
@@ -85,17 +98,28 @@ def postprocess(self, net_out, im, save = True):
 		x2 = right
 		y2 = bot
 		roi = im[y:y2, x:x2]
+		# saved_boxes.append([roi, image_copy])
+		saved_boxes.append(image_copy)
 
 		area = roi.shape[0] * roi.shape[1]
+
 		if area > max_area:
-			max_box = roi
+			# max_box = [roi, image_copy]
+			max_box = image_copy
 			max_area = area
 
 	global out_dir
-	cv2.imwrite((out_dir+"\\box_%d.jpg" % frame), max_box)
-	# cv2.imwrite(("C:\\Users\\grant\\Documents\\School\\Deep Learning\\Project\\DarkFlow\\darkflow\\video_results\\bounding_boxes\\box_%d.jpg" % frame), max_box)
-		# roi = im[0:h, 0:w]
-		# cv2.imwrite(("C:\\Users\\grant\\Documents\\School\\Deep Learning\\Project\\DarkFlow\\darkflow\\video_results\\Screen_%d.jpg" % img_num), roi)
+	os.makedirs(out_dir+"\\frame%d" % frame)
+	# cv2.imwrite((out_dir+"\\frame%d\\ground_box_frame%d.jpg" % (frame, frame)), max_box[0])
+	# cv2.imwrite((out_dir+"\\frame%d\\ground_box_frame_black%d.jpg" % (frame, frame)), max_box[1])
+	cv2.imwrite((out_dir+"\\frame%d\\ground_box_frame_black%d.jpg" % (frame, frame)), max_box)
+
+	for i, bs in enumerate(saved_boxes):
+		if bs is not max_box:
+			# cv2.imwrite((out_dir + "\\frame%d\\box%d.jpg" % (frame, i)), bs[0])
+			# cv2.imwrite((out_dir + "\\frame%d\\black_box%d.jpg" % (frame, i)), bs[1])
+			cv2.imwrite((out_dir + "\\frame%d\\black_box%d.jpg" % (frame, i)), bs)
+
 	if not save: return imgcv
 
 
